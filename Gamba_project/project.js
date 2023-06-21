865c9893377aea569713669e1cf5c38e2a7b6c67
phuongle841 <86511978+phuongle841@users.noreply.github.com>
2023-06-21 16:51:19 +0700

    Update project.js
    finish the last part of this small project

--- Gamba_project/project.js
+++ Gamba_project/project.js
@@ -57,10 +57,81 @@ const spin = () => {
             symbols.push(symbol);
         }
     }
-    console.log(symbols);
+    const reels = [];
+    for (let i = 0; i < COLS; i++) {
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random()*symbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.slice(randomIndex,1);
+        }        
+    }
+    return reels;
 };
+const transpose= (reels)=>{
+    const rows =[];
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);            
+        }
+    }
+    return rows;
+}
 
-// let balance = deposit();
-// const linesAmount = getNumberOfLines();
-// const bet = getBet(balance,linesAmount);
-spin();
+const printRows =(rows)=>{
+    for (const row of rows) {
+        let rowString ="";
+        for (const [i,symbol] of row.entries()) {
+            rowString +=symbol;
+            if (i!=row.length-1) {
+                rowString+=" | ";
+            }
+        }
+        console.log(rowString);
+    }
+}
+const getWinning=(rows, bet, lines)=>{
+    let winnings = 0;
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame =true;
+        for (const symbol of symbols) {
+            if (symbol!=symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }        
+        if (allSame) {
+            winnings += bet* SYMBOLS_VALUES[symbols[0]];
+        }
+    }
+    return winnings;
+}
+const game =()=>{
+    let balance = deposit();
+    while (true) {
+        console.log("you have a balance of "+ balance.toString());
+        const linesAmount = getNumberOfLines();
+        const bet = getBet(balance,linesAmount);
+        balance -=bet*linesAmount;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinning(rows,bet,linesAmount);
+        balance+=winnings;
+        console.log("you won, $"+winnings.toString());
+        if (balance<=0) {
+            console.log("you run out of money");
+            break;
+        }
+        const playAgain = prompt("do you want to play again ? (y/n) ");
+        if (playAgain!="y") {
+            break;
+        }
+    }
+    
+}
+game();
